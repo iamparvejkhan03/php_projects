90fcf0c27d658d0b92588c7a477fdd0d0d226c17
Parvej Khan <iamparvejkhan03@gmail.com>
2024-12-24 13:53:42 +0530

    removed my secret key from Google oauth

--- googlogBasic/config.php
+++ googlogBasic/config.php
@@ -1,8 +1,8 @@
 <?php
     require_once 'vendor/autoload.php';
     $google_client = new Google_Client();
-    $google_client->setClientId('782946008987-dlvuse5eec23necqjj286is7p4vs3g0q.apps.googleusercontent.com');
-    $google_client->setClientSecret('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');
+    $google_client->setClientId('');
+    $google_client->setClientSecret('');
     $google_client->setRedirectUri('http://localhost/Projects/php_projects/googlogBasic/index.php');
     $google_client->addScope('email');
     $google_client->addScope('profile');
