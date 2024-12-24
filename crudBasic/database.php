c5fedc25e8ca57a4d63dff12d7e9b811575785c1
Parvej Khan <iamparvejkhan03@gmail.com>
2024-12-24 14:54:32 +0530

    Update database.php

--- crudBasic/database.php
+++ crudBasic/database.php
@@ -1,8 +1,8 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
+    $db_server = "";
+    $db_user = "";
     $db_pass = "";
-    $db_name = "all_users_db";
+    $db_name = "";
     $conn = null;
 
     try{
